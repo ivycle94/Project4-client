e7322ea7c791915807961e107da6065a86dacba2
Ivycle94 <Ivycle94@gmail.com>
2022-04-28 08:46:28 -0500

    added patch modal file component for main model

---
+++ src/components/setup/EditSetupModal.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
+import SetupForm from '../shared/SetupForm'
+//import msg handling
+
+const EditSetupModal = (props) => {
+    const { user, show, handleClose, updateSetup, msgAlert, triggerRefresh } = props
+    const [setup, setSetup] = useState(props.setup)
+
+    const handleChange = (e) => {
+        // e === event
+        e.persist()
+
+        setSetup(prevSetup => {
+            const name = e.target.name
+            let value = e.target.value
+
+            console.log('etarget type', e.target.type)
+
+            if (e.target.type === 'number') {
+                value = parseInt(e.target.value)
+            }
+
+            const updatedValue = { [name]: value }
+
+            console.log('prevSetup', prevSetup)
+            console.log('updatedValue', updatedValue)
+
+            return { ...prevSetup, ...updatedValue }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        // e === event
+        e.preventDefault()
+
+        console.log('the spooky setup to submit', setup)
+        updateSetup(user, setup)
+            // if create is successful, we should navigate to the show page
+            .then(() => handleClose())
+            // then we send a success message
+            .then(() =>
+                msgAlert({
+                    // heading: 'Setup updated!',
+                    // message: editSetupSuccess,
+                    // variant: 'success',
+                }))
+            .then(() => triggerRefresh())
+            // if there is an error, we'll send an error message
+            .catch(() =>
+                msgAlert({
+                    // heading: 'Setup update failed!',
+                    // message: editSetupFailure,
+                    // variant: 'danger',
+                }))
+        // console.log('this is the patched setup', setup)
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body>
+                <SetupForm
+                    setup={setup}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                    heading="Edit Setup!"
+                />
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default EditSetupModal
