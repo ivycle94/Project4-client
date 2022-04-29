35f7ce06c346f77abf8b8a6e4d1da762cfd93c64
Ivycle94 <Ivycle94@gmail.com>
2022-04-29 18:27:58 -0500

    added app name

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -57,7 +57,7 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                react-auth-template
+                Bastion
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
